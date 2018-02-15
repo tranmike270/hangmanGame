bbc91e3f6603c1e7982702ac0923123ff0a15a14
Michael Tran <tranmike196@gmail.com>
2018-02-15 11:47:36 -0600

    Commented out stuff for volume bar temp.

--- hangman.js
+++ hangman.js
@@ -1449,47 +1449,47 @@ function stopSong() {
 
 // Creating volume bar
 
-$(function() {
+// $(function() {
  
-    var slider = $('#slider'),
-      tooltip = $('.tooltip');
+//     var slider = $('#slider'),
+//       tooltip = $('.tooltip');
    
-    tooltip.hide();
+//     tooltip.hide();
    
-    slider.slider({
-      range: "min",
-      min: 1,
-      value: 35,
+//     slider.slider({
+//       range: "min",
+//       min: 1,
+//       value: 35,
    
-      start: function(event,ui) {
-        tooltip.fadeIn('fast');
-      },
+//       start: function(event,ui) {
+//         tooltip.fadeIn('fast');
+//       },
    
-      slide: function(event, ui) {
+//       slide: function(event, ui) {
    
-        var value = slider.slider('value'),
-          volume = $('.volume');
+//         var value = slider.slider('value'),
+//           volume = $('.volume');
    
-        tooltip.css('left', value).text(ui.value);
+//         tooltip.css('left', value).text(ui.value);
    
-        if(value <= 5) { 
-          volume.css('background-position', '0 0');
-        } 
-        else if (value <= 25) {
-          volume.css('background-position', '0 -25px');
-        } 
-        else if (value <= 75) {
-          volume.css('background-position', '0 -50px');
-        } 
-        else {
-          volume.css('background-position', '0 -75px');
-        };
+//         if(value <= 5) { 
+//           volume.css('background-position', '0 0');
+//         } 
+//         else if (value <= 25) {
+//           volume.css('background-position', '0 -25px');
+//         } 
+//         else if (value <= 75) {
+//           volume.css('background-position', '0 -50px');
+//         } 
+//         else {
+//           volume.css('background-position', '0 -75px');
+//         };
    
-      },
+//       },
    
-      stop: function(event,ui) {
-        tooltip.fadeOut('fast');
-      },
-    });
+//       stop: function(event,ui) {
+//         tooltip.fadeOut('fast');
+//       },
+//     });
    
-  });
+//   });
