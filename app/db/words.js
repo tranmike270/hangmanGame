002ba9adf82537a2d27f8deab784d04700f94380
Michael Tran <tranmike196@gmail.com>
2018-05-08 09:33:24 -0500

    Fixed error in words js file

--- app/db/words.js
+++ app/db/words.js
@@ -346,7 +346,7 @@
         "description": {
             "titles": ["Re:Creators"],
             "synopsis": "Humans have designed countless worlds—each one born from the unique imagination of its creator. Souta Mizushino is a high school student who aspires to be such a creator by writing and illustrating his own light novel. One day, while watching anime for inspiration, he is briefly transported into a fierce fight scene. When he returns to the real world, he realizes something is amiss: the anime's headstrong heroine, Celestia Yupitilia, has somehow returned with him. Before long, other fictional characters appear in the world, carrying the hopes and scars of their home. A princely knight, a magical girl, a ruthless brawler, and many others now crowd the streets of Japan. However, the most mysterious one is a woman in full military regalia, dubbed 'Gunpuku no Himegimi,' who knows far more than she should about the creators' world. Despite this, no one knows her true name or the world she is from. Meanwhile, Souta and Celestia work together with Meteora Österreich, a calm and composed librarian NPC, to uncover the meaning behind these unnatural events. With powerful forces at play, the once clear line between reality and imagination continues to blur, leading to a fateful meeting between creators and those they created.",
-            "characters": ["Altair","Meteora","	Upitiria, Selesia","Mizushino, Souta"]
+            "characters": ["Altair","Meteora","Upitiria, Selesia","Mizushino, Souta"]
         }
     }
 ]
